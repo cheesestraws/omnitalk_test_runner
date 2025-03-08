dc713d22360291181ddaaf607ce2a42e32ba8159
cheesestraws <rob@midworld.co.uk>
2025-03-08 16:38:01 +0000

    tests: include build logs in log output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo >> main/tunables.h
 echo '#define TESTS_REBOOT_AFTERWARDS' >> main/tunables.h
 echo '#define TESTS_NO_HANG_ON_FAIL' >> main/tunables.h
 
-idf.py build
+idf.py build | tee /github/workspace/run.log
 cd build 
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
-timeout 5m /opt/qemu/bin/qemu-system-xtensa -machine esp32 -nographic -no-reboot -watchdog-action shutdown -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial mon:stdio | tee /github/workspace/run.log
+timeout 5m /opt/qemu/bin/qemu-system-xtensa -machine esp32 -nographic -no-reboot -watchdog-action shutdown -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial mon:stdio | tee -a /github/workspace/run.log
