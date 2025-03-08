0db701999f4e4775c1f693f4c09fcaa08bd8b4d1
cheesestraws <rob@midworld.co.uk>
2025-03-08 16:11:11 +0000

    tests: cat output file

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,5 @@ idf.py build
 cd build 
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 timeout 5m /opt/qemu/bin/qemu-system-xtensa -machine esp32 -nographic -no-reboot -watchdog-action shutdown -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial mon:stdio | tee /github/workspace/run.log
+echo
+cat /github/workspace/run.log
