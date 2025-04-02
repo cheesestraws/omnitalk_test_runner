8c0d3c36e52a308a553b95a2ac2cc41e5d85c3a6
cheesestraws <rob@midworld.co.uk>
2025-04-02 20:13:16 +0100

    tests: experiment with qemu monitor

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ echo '#define TESTS_NO_HANG_ON_FAIL' >> main/tunables.h
 echo '#define RUN_TESTS' >> main/tunables.h
 
 idf.py build | tee /github/workspace/run.log
+idf.py qemu monitor
 cd build 
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 timeout 5m /opt/qemu/bin/qemu-system-xtensa -machine esp32 -nographic -no-reboot -watchdog-action shutdown -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial mon:stdio | tee -a /github/workspace/run.log
